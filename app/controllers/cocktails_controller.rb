2de02be8a6d2e6a8bdfb6afb485614512d84de5e
Jean Pierre Symoens <jpsymoens0@gmail.com>
2020-11-19 13:55:56 +0100

    Created Cocktail MVC

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,34 @@
+class CocktailsController < ApplicationController
+  before_action :set_cocktails, only: %i[edit show]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktails_params)
+
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_cocktails
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktails_params
+    params.require(:cocktail).permit(:name)
+  end
+end
