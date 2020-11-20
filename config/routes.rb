ddb7a1b420cef3cf17784f1c1fe0ee732a0bcd97
Jean Pierre Symoens <jpsymoens0@gmail.com>
2020-11-20 12:19:47 +0100

    Fix routing issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'cocktails#index'
-  resources :cocktails, only: %i[new create show] do
+  root to: 'cocktails#index'
+  resources :cocktails, only: %i[index new create show] do
     resources :doses, only: %i[create new]
   end
   resources :doses, only: [:destroy]
